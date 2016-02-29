934115171d149821e424b942ce40b7dfd653386e
Neo <neolwc@gmail.com>
2016-02-29 15:33:35 -0500

    Delete hard-coded data

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -75,11 +75,6 @@ var Comment = React.createClass({
 	}
 });
 
-var data = [
-	{id: 1, author: "Pete Hunt", text: "This is one comment"},
-	{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 ReactDOM.render(
 	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
