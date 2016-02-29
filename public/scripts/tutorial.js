5e415d458b51a5dd360fcdc820c4bec418743adf
Neo <neolwc@gmail.com>
2016-02-29 15:23:58 -0500

    Reactive state

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,9 +1,29 @@
 var CommentBox = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
@@ -61,6 +81,6 @@ var data = [
 ];
 
 ReactDOM.render(
-	<CommentBox url="/api/comments" />,
+	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
